5176152bafe43370bc521ba7a874f7de1c3dbb7e
baz <wanghe0824@gmail.com>
2019-03-30 01:06:33 -0400

    add template

---
+++ cpp/ch8/temp.cpp
@@ -0,0 +1,34 @@
+/*
+  temp.cpp
+  Created by: baz
+  Date: 2019-03-30 00:58
+*/
+#include <iostream>
+
+template <typename T> void Swap(T &a, T &b);
+
+int main() {
+  using namespace std;
+  int i = 10;
+  int j = 20;
+  cout << "i, j = " << i << ", " << j << ".\n";
+  cout << "Using compile-generated int swapper:\n";
+  Swap(i, j);
+  cout << "Now i, j = " << i << ", " << j << ".\n";
+
+  double x = 24.5;
+  double y = 81.7;
+  cout << "x, y = " << x << ", " << y << ".\n";
+  cout << "Using compile-generated double wapper:\n";
+  Swap(x, y);
+  cout << "Now x, y = " << x << ", " << y << ".\n";
+
+  return 0;
+}
+
+template <typename T> void Swap(T &a, T &b) {
+  T temp;
+  temp = a;
+  a = b;
+  b = temp;
+}
